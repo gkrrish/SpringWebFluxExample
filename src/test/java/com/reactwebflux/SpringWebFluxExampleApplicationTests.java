e30cd7ef1ed0a384a6c880c5bf02082b33fe1ce3
Krishna <gkrrish.11@gmail.com>
2024-04-05 10:30:50 +0530

    initial commit

---
+++ src/test/java/com/reactwebflux/SpringWebFluxExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFluxExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
