3301626829ffc847f588a2b74dca827a3fe7b8c8
Krishna <gkrrish.11@gmail.com>
2024-04-06 13:08:11 +0530

    Prometheus added dependency and created the Reactive db in Postgres

--- src/main/java/com/reactwebflux/SpringWebFluxExampleApplication.java
+++ src/main/java/com/reactwebflux/SpringWebFluxExampleApplication.java
@@ -8,6 +8,10 @@ public class SpringWebFluxExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebFluxExampleApplication.class, args);
+		System.out.println("http://localhost:9090");
+		String listOfEndPoints ="http://localhost:8287/actuator/prometheus"+
+								"http://localhost:8287/actuator/health"+
+								"http://localhost:8287/actuator";
 	}
 
 }
